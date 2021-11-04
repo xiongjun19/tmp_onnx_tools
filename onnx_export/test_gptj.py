057496fbcfbfa2efb6c44f6b1760c33cd92f2a5e
Ubuntu <ubuntu@ip-172-31-9-92.us-west-2.compute.internal>
2021-11-04 16:51:57 +0800

    changing the test_gptj

--- onnx_export/test_gptj.py
+++ onnx_export/test_gptj.py
@@ -3,8 +3,20 @@
 from transformers import AutoTokenizer
 from transformers import AutoModelForCausalLM
 
-pretrain_name = "EleutherAI/gpt-j-6B"
 
-tokenizer = AutoTokenizer.from_pretrained(pretrain_name)
-model = AutoModelForCausalLM.from_pretrained(pretrain_name)
-print(type(model))
+def test(pretrain_name=None):
+    if pretrain_name is None:
+        pretrain_name = "EleutherAI/gpt-j-6B"
+    tokenizer = AutoTokenizer.from_pretrained(pretrain_name)
+    model = AutoModelForCausalLM.from_pretrained(pretrain_name)
+    print(type(model))
+    print(type(tokenizer))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        test(sys.argv[1])
+    else:
+        test()
+    print("done")
