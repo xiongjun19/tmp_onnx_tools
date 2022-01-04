9510b6951ff0728398759ed954bb0ea1b25426e8
Ubuntu <ubuntu@ip-172-31-19-172.us-west-2.compute.internal>
2022-01-04 10:02:39 +0000

    adding gpt_gen command

---
+++ onnx_export/scripts/gpt_gen.sh
@@ -0,0 +1,6 @@
+# tooks about 20minutes 79GB Memory; and about 150GB disk
+python gpt_serial_onnx.py -o ../../onnx_models/gpt_18b/ -m ../model_configs/gpt_18b
+
+
+# tooks about 30minutes 150GB Memory;  and about 150GB disk
+python gpt_serial_onnx.py -o ../../onnx_models/gpt_39b/ -m ../model_configs/gpt_39b
